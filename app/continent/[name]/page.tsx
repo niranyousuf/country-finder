bb734197304039609d86a23b031594460d357b6e
Niran Yousuf <niran069@gmail.com>
2025-05-03 10:02:44 +0600

    issue fix

--- app/continent/[name]/page.tsx
+++ app/continent/[name]/page.tsx
@@ -1,19 +1,24 @@
 import CountryList from '@/components/CountryList'
 
 type Props = {
-  name: string
+  params: {
+    name: string
+  }
   searchParams?: { [key: string]: string | string[] | undefined }
 }
-export default function ContinentPage({ params: { name } }: { params: Props }) {
-  const continentName = name;
-  const formattedName = continentName.charAt(0).toUpperCase() + continentName.slice(1);
+type PageProps = {
+  params: { [key: string]: string }
+}
+export default function ContinentPage({ params }: Props) {
+  const continentName = params.name;
+  const formattedName = params.name.charAt(0).toUpperCase() + params.name.slice(1);
 
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-4xl font-bold text-center mb-12">
         Countries in {formattedName}
       </h1>
-      <CountryList searchTerm="" continent={continentName} />
+      <CountryList searchTerm="" continent={params.name} />
     </div>
   )
 }
